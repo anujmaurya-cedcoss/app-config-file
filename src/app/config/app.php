008f8aab3de5d8c77b12df051dec14cab92c4207
Anuj Maurya <anujmaurya@cedcoss.com>
2023-06-26 10:48:05 +0530

    done

--- src/app/config/app.php
+++ src/app/config/app.php
@@ -2,11 +2,13 @@
 
 date_default_timezone_set("Asia/Calcutta");
 define(
-    "MYAPP",
-    "['name'=>'Phalcon',
-    'version'=>'4.0.2',
-    'time'=>" . date('h-i-sa') . ",
-    'timezone'=>'Asia/Calcutta',
-    'language'=>'English']"
+    "myApp",
+    "[
+        'name'=>'Config File App',
+        'version'=>'1.1.1',
+        'time'=>" . date('h-i-sa') . ",
+        'timezone'=>'Asia/Calcutta',
+        'language'=>'English'
+    ]"
 );
-return MYAPP;
+return myApp;
